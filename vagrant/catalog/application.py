c35cbf42af1c16d712aa2066bd24a2784e1bd030
Eric Hall <ericmbf@gmail.com>
2019-01-23 18:33:44 -0300

    Organize code comments

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -29,9 +29,7 @@ session = DBSession()
 CLIENT_ID = \
     json.loads(open('./client_secrets.json', 'r').read())['web']['client_id']
 
-
-# Create anti-forgery state token
-
+# Implement anti-forgery state token
 @app.route('/login')
 def showLogin():
     state = ''.join(
@@ -40,8 +38,6 @@ def showLogin():
     login_session['state'] = state
     return render_template('login.html', STATE=state)
 
-
-# Disconnect based on provider
 
 @app.route('/disconnect')
 def disconnect():
@@ -54,7 +50,6 @@ def disconnect():
 def gconnect():
 
     # Validate state token
-
     if request.args.get('state') != login_session['state']:
         response = make_response(
             json.dumps('Invalid state parameter.'), 401)
@@ -62,13 +57,10 @@ def gconnect():
         return response
 
     # Obtain authorization code
-
     code = request.data
 
     try:
-
         # Upgrade the authorization code into a credentials object
-
         oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
@@ -79,7 +71,6 @@ def gconnect():
         return response
 
     # Check that the access token is valid.
-
     access_token = credentials.access_token
     url = \
         'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' \
@@ -88,14 +79,12 @@ def gconnect():
     result = json.loads(h.request(url, 'GET')[1])
 
     # If there was an error in the access token info, abort.
-
     if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
         response.headers['Content-Type'] = 'application/json'
         return response
 
     # Verify that the access token is used for the intended user.
-
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
         response = make_response(
@@ -104,7 +93,6 @@ def gconnect():
         return response
 
     # Verify that the access token is valid for this app.
-
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
@@ -116,12 +104,10 @@ def gconnect():
     stored_gplus_id = login_session.get('gplus_id')
 
     # Store the access token in the session for later use.
-
     login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
 
     # Get user info
-
     userinfo_url = 'https://www.googleapis.com/oauth2/v1/userinfo'
     params = {'access_token': credentials.access_token, 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
@@ -131,13 +117,9 @@ def gconnect():
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
-
-    # ADD PROVIDER TO LOGIN SESSION
-
     login_session['provider'] = 'google'
 
     # see if user exists, if it doesn't make a new one
-
     user_id = getUserID(data['email'])
     if not user_id:
         user_id = createUser(login_session)
@@ -165,8 +147,6 @@ def gconnect():
     print 'done!'
     return output
 
-
-# User Helper Functions
 
 def createUser(login_session):
     newUser = User(username=login_session['username'],
