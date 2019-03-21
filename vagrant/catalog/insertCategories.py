a722e935edb10a4e56bafd25ed8b67a43e10e0bb
Eric Hall <ericmbf@gmail.com>
2019-03-21 20:34:03 -0300

    Change database from insert data

--- vagrant/catalog/insertCategories.py
+++ vagrant/catalog/insertCategories.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from models import Base, Category, Item
 
-engine = create_engine('sqlite:///categoryitems.db')
+engine = create_engine('postgresql://catalog:password@localhost/catalog')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
