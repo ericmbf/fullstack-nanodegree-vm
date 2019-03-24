fc1833b7c335736743fcf30856259828bbc58704
Eric Hall <ericmbf@gmail.com>
2019-03-24 16:49:51 -0300

    Fix google oauth2 after gplus to be deprecated

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -18,6 +18,8 @@ import requests
 from flask_httpauth import HTTPBasicAuth
 from flask_wtf.csrf import CSRFProtect, CSRFError
 from functools import wraps
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
 
 app = Flask(__name__)
@@ -77,82 +79,23 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Obtain authorization code
-    code = request.data
+    token = request.args.get('id_token')
 
     try:
-        # Upgrade the authorization code into a credentials object
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
-        oauth_flow.redirect_uri = 'postmessage'
-        credentials = oauth_flow.step2_exchange(code)
-    except FlowExchangeError:
+        data = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+    except:
         response = make_response(
-            json.dumps('Failed to upgrade the authorization code.'), 401)
+            json.dumps('Invalid oauth authentication.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Check that the access token is valid.
-    access_token = credentials.access_token
-    url = \
-        'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' \
-        % access_token
-    h = httplib2.Http()
-    result = json.loads(h.request(url, 'GET')[1])
-
-    # If there was an error in the access token info, abort.
-    if result.get('error') is not None:
-        response = make_response(json.dumps(result.get('error')), 500)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    # Verify that the access token is used for the intended user.
-    gplus_id = credentials.id_token['sub']
-    if result['user_id'] != gplus_id:
-        response = make_response(
-            json.dumps("Token's user ID doesn't match given user ID."), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    # Verify that the access token is valid for this app.
-    if result['issued_to'] != CLIENT_ID:
-        response = make_response(
-            json.dumps("Token's client ID does not match app's."), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    stored_access_token = login_session.get('access_token')
-    stored_gplus_id = login_session.get('gplus_id')
-
-    # Store the access token in the session for later use.
-    login_session['access_token'] = credentials.access_token
-    login_session['gplus_id'] = gplus_id
-
-    # Get user info
-    userinfo_url = 'https://www.googleapis.com/oauth2/v1/userinfo'
-    params = {'access_token': credentials.access_token, 'alt': 'json'}
-    answer = requests.get(userinfo_url, params=params)
-
-    data = answer.json()
-
+    # Specify the CLIENT_ID of the app that accesses the backend:
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
     login_session['provider'] = 'google'
 
-    # see if user exists, if it doesn't make a new one
-    user_id = getUserID(data['email'])
-    if not user_id:
-        user_id = createUser(login_session)
-    login_session['user_id'] = user_id
-
-    # Check if the acess token is valid
-    if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(
-            json.dumps('Current user is already connected.'), 200)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    return json.dumps({'success': True}), 200, {
+    return json.dumps({'success': true}), 200, {
         'ContentType': 'application/json'}
 
 
